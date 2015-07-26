c53bf8812f7dab054838701a7a46d28e07653821
LM <ralphtheninja@riseup.net>
2015-07-26 02:45:58 +0200

    skip build if resulting .tar.gz already exists

--- bin.js
+++ bin.js
@@ -112,15 +112,24 @@ function prebuild (v, cb) {
   setupLog('Preparing to prebuild ' + pkg.name + '@' + pkg.version + ' for ' + v + ' on ' + rc.platform + '-' + rc.arch)
   getAbi(v, function (err, abi) {
     if (err) return log.error(err.message)
-    build(v, function (err, filename) {
-      if (err) return log.error(err.message)
-      pack(filename, abi)
+    var tarPath = getTarPath(abi)
+    var next = function (err) {
+      if (err) return cb(err)
+      cb(null, { path: tarPath, abi: abi, version: v, platform: rc.platform, arch: rc.arch})
+    }
+    fs.stat(tarPath, function (err, st) {
+      if (!err) {
+        log.info(tarPath + ' exists, skipping build')
+        return next()
+      }
+      build(v, function (err, filename) {
+        if (err) return cb(err)
+        pack(filename, tarPath, next)
+      })
     })
   })
 
-  function pack (filename, abi) {
-    var name = pkg.name + '-v' + pkg.version + '-node-v' + abi + '-' + rc.platform + '-' + rc.arch + '.tar.gz'
-    var tarPath = path.join('prebuilds', name)
+  function pack (filename, tarPath, cb) {
     fs.mkdir('prebuilds', function () {
       fs.stat(filename, function (err, st) {
         if (err) return cb(err)
@@ -141,9 +150,14 @@ function prebuild (v, cb) {
 
         pack.pipe(zlib.createGzip()).pipe(ws).on('close', function () {
           log.info('package', 'Prebuild written to ' + tarPath)
-          cb(null, {path: tarPath, name: name, abi: abi, version: v, platform: rc.platform, arch: rc.arch})
+          cb()
         })
       })
     })
   }
+}
+
+function getTarPath (abi) {
+  var name = pkg.name + '-v' + pkg.version + '-node-v' + abi + '-' + rc.platform + '-' + rc.arch + '.tar.gz'
+  return path.join('prebuilds', name)
 }
