4e3a85e167c4c7a05832c9bb4d1c4b9987447cea
LM <ralphtheninja@riseup.net>
2015-07-28 01:20:21 +0200

    move compile property defaults to rc properties

--- rc.js
+++ rc.js
@@ -3,7 +3,8 @@ var minimist = require('minimist')
 module.exports = require('rc')('prebuild', {
   target: process.version,
   arch: process.arch,
-  platform: process.platform
+  platform: process.platform,
+  compile: (process.env.npm_config_argv && process.env.npm_config_argv.indexOf('--build-from-source') > -1)
 }, minimist(process.argv, {
   alias: {
     target: 't',
@@ -17,9 +18,5 @@ module.exports = require('rc')('prebuild', {
     'build-from-source': 'compile',
     compile: 'c',
     preinstall: 'i'
-  },
-  default: {
-    // this is hackish - whats the correct way of doing this?
-    compile: !!process.env.prebuild_compile || (process.env.npm_config_argv && process.env.npm_config_argv.indexOf('--build-from-source') > -1)
   }
 }))
