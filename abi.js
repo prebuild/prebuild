eb095d06bcb7bc8dad38044db47326516d30af8d
Lars-Magnus Skog <ralphtheninja@riseup.net>
2015-08-31 15:34:40 +0200

    test abi and add failing test

--- abi.js
+++ abi.js
@@ -1,13 +1,15 @@
-var install = require('node-gyp-install')
-var readGypFile = require('./util').readGypFile
+var nodeGypInstall = require('node-gyp-install')
+var util = require('./util')
 
 function getAbi (opts, version, cb) {
   var log = opts.log
+  var install = opts.install || nodeGypInstall
   version = version.replace('v', '')
-  readGypFile(version, 'src/node_version.h', function (err, a) {
+
+  util.readGypFile(version, 'src/node_version.h', function (err, a) {
     if (err && err.code === 'ENOENT') return retry()
     if (err) return cb(err)
-    readGypFile(version, 'src/node.h', function (err, b) {
+    util.readGypFile(version, 'src/node.h', function (err, b) {
       if (err && err.code === 'ENOENT') return retry()
       if (err) return cb(err)
       var abi = parse(a) || parse(b)
