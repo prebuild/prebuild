f71f89f8ae989084e2074aca42a143562e63a82c
Brett Lawson <brett19@gmail.com>
2015-09-22 11:56:48 -0300

    Do not loop indefinitely when doing getAbi.
    Previously a call to getAbi where the node.h and node_version.h
    files cannot be found (due to a download failure or logic error)
    would cause prebuild to loop indefinitely trying to download that
    release.  This causes prebuild to only attempt to download the
    files a single time before failing.

--- abi.js
+++ abi.js
@@ -5,22 +5,34 @@ function getAbi (opts, version, cb) {
   var log = opts.log
   var install = opts.install || gypinstall
   version = version.replace('v', '')
-  util.readGypFile(version, 'node_version.h', function (err, a) {
-    if (err && err.code === 'ENOENT') return retry()
-    if (err) return cb(err)
-    util.readGypFile(version, 'node.h', function (err, b) {
-      if (err && err.code === 'ENOENT') return retry()
-      if (err) return cb(err)
-      var abi = parse(a) || parse(b)
-      if (!abi) return cb(new Error('Could not detect abi for ' + version))
-      cb(null, abi)
-    })
+
+  tryReadFiles(function(err, abi) {
+    if (err && err.code === 'ENOENT') {
+      
+      return install({log: log, force: true}, version, function (err) {
+        if (err) return cb(err)
+
+        tryReadFiles(function(err, abi) {
+          if (err && err.code === 'ENOENT') {
+            return cb(new Error('Failed to locate `node.h` and `node_version.h`.'));
+          }
+          cb(err, abi)
+        })
+      })
+    }
+
+    cb(err, abi)
   })
 
-  function retry () {
-    install({log: log, force: true}, version, function (err) {
-      if (err) return cb(err)
-      getAbi(opts, version, cb)
+  function tryReadFiles(readCb) {
+    util.readGypFile(version, 'node_version.h', function (err, a) {
+      if (err) return readCb(err)
+      util.readGypFile(version, 'node.h', function (err, b) {
+        if (err) return readCb(err)
+        var abi = parse(a) || parse(b)
+        if (!abi) return readCb(new Error('Could not detect abi for ' + version))
+        readCb(null, abi)
+      })
     })
   }
 
