41dfbfce8651e16d8bd0c30335d8699ceebacab3
Brett Lawson <brett19@gmail.com>
2015-09-25 23:22:01 -0300

    Fixes #21: Ensure that the npm-cache folder exists.

--- download.js
+++ download.js
@@ -30,27 +30,31 @@ function downloadPrebuild (opts, cb) {
   })
 
   function download () {
-    fs.exists(cachedPrebuild, function (exists) {
-      if (exists) return unpack()
+    ensureNpmCacheDir(function (err) {
+      if (err) return onerror(err)
 
-      log.http('request', 'GET ' + downloadUrl)
-      var req = get(downloadUrl, function (err, res) {
-        if (err) return onerror(err)
-        log.http(res.statusCode, downloadUrl)
-        if (res.statusCode !== 200) return onerror()
-        fs.mkdir(util.prebuildCache(), function () {
-          pump(res, fs.createWriteStream(tempFile), function (err) {
-            if (err) return onerror(err)
-            fs.rename(tempFile, cachedPrebuild, function (err) {
-              if (err) return cb(err)
-              unpack()
+      fs.exists(cachedPrebuild, function (exists) {
+        if (exists) return unpack()
+
+        log.http('request', 'GET ' + downloadUrl)
+        var req = get(downloadUrl, function (err, res) {
+          if (err) return onerror(err)
+          log.http(res.statusCode, downloadUrl)
+          if (res.statusCode !== 200) return onerror()
+          fs.mkdir(util.prebuildCache(), function () {
+            pump(res, fs.createWriteStream(tempFile), function (err) {
+              if (err) return onerror(err)
+              fs.rename(tempFile, cachedPrebuild, function (err) {
+                if (err) return cb(err)
+                unpack()
+              })
             })
           })
         })
-      })
 
-      req.setTimeout(30 * 1000, function () {
-        req.abort()
+        req.setTimeout(30 * 1000, function () {
+          req.abort()
+        })
       })
 
       function onerror (err) {
@@ -82,6 +86,32 @@ function downloadPrebuild (opts, cb) {
       }
       cb(new Error('Missing .node file in archive'))
     })
+  }
+
+  function ensureNpmCacheDir (cb) {
+    var cacheFolder = util.npmCache()
+    if (fs.access) {
+      fs.access(cacheFolder, fs.R_OK | fs.W_OK, function (err) {
+        if (err && err.code === 'ENOENT') {
+          return makeNpmCacheDir()
+        }
+        if (err) return cb(err)
+        cb()
+      })
+    } else {
+      fs.exists(cacheFolder, function (exists) {
+        if (!exists) return makeNpmCacheDir()
+        cb()
+      })
+    }
+
+    function makeNpmCacheDir () {
+      log.info('npm cache directory missing, creating it...')
+      fs.mkdir(cacheFolder, function (err) {
+        if (err) return cb(err)
+        cb()
+      })
+    }
   }
 }
 
--- test/download-test.js
+++ test/download-test.js
@@ -12,7 +12,7 @@ var build = path.join(__dirname, 'build')
 var unpacked = path.join(build, 'Release/leveldown.node')
 
 test('downloading from GitHub, not cached', function (t) {
-  t.plan(19)
+  t.plan(20)
   rm.sync(build)
   rm.sync(util.prebuildCache())
 
@@ -40,15 +40,31 @@ test('downloading from GitHub, not cached', function (t) {
 
   var downloadUrl = util.getDownloadUrl(opts)
   var cachedPrebuild = util.cachedPrebuild(downloadUrl)
+  var npmCache = util.npmCache()
   var tempFile
 
+  var existsCallNum = 0
+  var _access = fs.access ? fs.access.bind(fs) : fs.access
   var _exists = fs.exists.bind(fs)
+  if (_access) {
+    fs.access = function (path, a, cb) {
+      if (existsCallNum++ === 0) {
+        t.equal(path, npmCache, 'fs.exists called for npm cache')
+        _access(path, cb)
+      }
+    }
+  }
   fs.exists = function (path, cb) {
-    t.equal(path, cachedPrebuild, 'fs.exists called')
-    _exists(path, function (exists) {
-      t.equal(exists, false, 'prebuild should not be cached')
-      cb(exists)
-    })
+    if (existsCallNum++ === 0) {
+      t.equal(path, npmCache, 'fs.exists called for npm cache')
+      _exists(path, cb)
+    } else {
+      t.equal(path, cachedPrebuild, 'fs.exists called for prebuild')
+      _exists(path, function (exists) {
+        t.equal(exists, false, 'prebuild should be cached')
+        cb(exists)
+      })
+    }
   }
 
   var mkdirCount = 0
@@ -95,6 +111,7 @@ test('downloading from GitHub, not cached', function (t) {
     t.equal(fs.existsSync(unpacked), true, unpacked + ' should exist')
     t.equal(fs.existsSync(tempFile), false, 'temp file should be gone')
     fs.exists = _exists
+    fs.access = _access
     fs.mkdir = _mkdir
     fs.createWriteStream = _createWriteStream
     fs.createReadStream = _createReadStream
@@ -102,7 +119,7 @@ test('downloading from GitHub, not cached', function (t) {
 })
 
 test('cached prebuild', function (t) {
-  t.plan(9)
+  t.plan(10)
   rm.sync(build)
 
   var opts = {
@@ -119,14 +136,30 @@ test('cached prebuild', function (t) {
 
   var downloadUrl = util.getDownloadUrl(opts)
   var cachedPrebuild = util.cachedPrebuild(downloadUrl)
+  var npmCache = util.npmCache()
 
+  var existsCallNum = 0
+  var _access = fs.access ? fs.access.bind(fs) : fs.access
   var _exists = fs.exists.bind(fs)
+  if (_access) {
+    fs.access = function (path, a, cb) {
+      if (existsCallNum++ === 0) {
+        t.equal(path, npmCache, 'fs.exists called for npm cache')
+        _access(path, cb)
+      }
+    }
+  }
   fs.exists = function (path, cb) {
-    t.equal(path, cachedPrebuild, 'fs.exists called')
-    _exists(path, function (exists) {
-      t.equal(exists, true, 'prebuild should be cached')
-      cb(exists)
-    })
+    if (existsCallNum++ === 0) {
+      t.equal(path, npmCache, 'fs.exists called for npm cache')
+      _exists(path, cb)
+    } else {
+      t.equal(path, cachedPrebuild, 'fs.exists called for prebuild')
+      _exists(path, function (exists) {
+        t.equal(exists, true, 'prebuild should be cached')
+        cb(exists)
+      })
+    }
   }
 
   var _createWriteStream = fs.createWriteStream.bind(fs)
@@ -149,6 +182,7 @@ test('cached prebuild', function (t) {
     fs.createReadStream = _createReadStream
     fs.createWriteStream = _createWriteStream
     fs.exists = _exists
+    fs.access = _access
   })
 })
 
