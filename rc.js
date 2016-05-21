8ee98f37c24f0a623968fc51ec66737231cc9d5d
Elijah Insua <tmpvar@gmail.com>
2016-05-20 21:23:02 -0400

    make distributed files regex configurable
    This allows people to build / distribute platform specific files,
    rather than only the *.node artifact. This is helpful for including
    things like statically linked archives (*.a) or similar platform
    specific build artifacts.

--- rc.js
+++ rc.js
@@ -38,7 +38,8 @@ var rc = module.exports = require('rc')('prebuild', {
   path: '.',
   backend: 'node-gyp',
   proxy: process.env['HTTP_PROXY'],
-  'https-proxy': process.env['HTTPS_PROXY']
+  'https-proxy': process.env['HTTPS_PROXY'],
+  'collect-files-filter': '\\.node$'
 }, minimist(process.argv, {
   alias: {
     target: 't',
@@ -76,6 +77,8 @@ if (rc.all === true) {
 if (rc['upload-all']) {
   rc.upload = rc['upload-all']
 }
+
+rc['collect-files-filter'] = new RegExp(rc['collect-files-filter'], 'i')
 
 if (!module.parent) {
   console.log(JSON.stringify(module.exports, null, 2))
