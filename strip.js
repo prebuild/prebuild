6e370e2e75e5c2719d4576dd8d5a77a403badd93
Ole André Vadla Ravnås <oleavr@gmail.com>
2016-10-25 22:06:38 +0200

    fix regression in the strip implementation

--- strip.js
+++ strip.js
@@ -1,10 +1,12 @@
+var quote = require('shell-quote').quote
 var util = require('./util')
 
 function strip (file, cb) {
   // TODO no support on windows, noop
   var platform = util.platform()
   if (platform === 'win32') return process.nextTick(cb)
-  util.spawn('strip', stripArgs(platform, file), cb)
+  var argv = ['strip'].concat(stripArgs(platform, file))
+  util.spawn(quote(argv), cb)
 }
 
 function stripArgs (platform, file) {
