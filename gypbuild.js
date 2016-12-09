142c5e899048f103131364e7a9276e8089dc4ace
Lukas Geiger <lukas.geiger94@gmail.com>
2016-12-09 12:41:35 +0100

    Use node-gyp from npm

--- gypbuild.js
+++ gypbuild.js
@@ -1,4 +1,3 @@
-var gyp = require('./gyp')
 var util = require('./util')
 
 function runGyp (opts, target, cb) {
@@ -12,14 +11,7 @@ function runGyp (opts, target, cb) {
   })
 
   function run () {
-    var args = ['node', 'index.js']
-    if (opts.backend === 'node-ninja') {
-      args.push('configure')
-      args.push('build')
-      args.push('--builddir=build/' + target)
-    } else {
-      args.push('rebuild')
-    }
+    var args = ['node-gyp', 'rebuild']
     args.push('--target=' + target)
     args.push('--target_arch=' + opts.arch)
     if (opts.runtime === 'electron') {
@@ -28,27 +20,16 @@ function runGyp (opts, target, cb) {
     }
     if (opts.debug) args.push('--debug')
 
-    gyp({
-      gyp: opts.gyp,
-      backend: opts.backend,
-      log: opts.log,
-      args: args,
-      filter: function (command) {
-        if (command.name === 'configure') {
-          return configurePreGyp(command, opts)
-        }
+    var cmd = args.join(' ')
+    log.verbose('build', cmd)
+    util.exec(cmd, function (err) {
+      if (err) {
+        log.error('build', 'rebuild failed')
+        log.error('stack', err.stack)
+        return cb(err)
       }
-    }, cb)
-  }
-}
-
-function configurePreGyp (command, opts) {
-  var binary = opts.pkg.binary
-  if (binary && binary.module_name) {
-    command.args.push('-Dmodule_name=' + binary.module_name)
-  }
-  if (binary && binary.module_path) {
-    command.args.push('-Dmodule_path=' + binary.module_path)
+      cb()
+    })
   }
 }
 
