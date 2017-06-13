eccdef36b14d01f879753d95c5178527ac4b593c
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-06-13 17:40:19 +0200

    Allow strip program to be overridden
    Through the STRIP environment variable. This is sometimes necessary when
    cross-compiling, and is consistent with how gyp allows e.g. CXX to be set
    to override the C++ compiler used. The name STRIP was chosen as autotools
    also uses it to override its auto-detection.

--- strip.js
+++ strip.js
@@ -4,7 +4,7 @@ function strip (file, cb) {
   // TODO no support on windows, noop
   var platform = util.platform()
   if (platform === 'win32') return process.nextTick(cb)
-  util.spawn('strip', stripArgs(platform, file), cb)
+  util.spawn(process.env.STRIP || 'strip', stripArgs(platform, file), cb)
 }
 
 function stripArgs (platform, file) {
