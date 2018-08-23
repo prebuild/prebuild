2e5de62c5777f842d74104a809529e572a6db259
Jim Schlight <jim@inspiredware.com>
2018-08-22 17:25:14 -0700

    Removes dead code
    Also includes merge from prebuild/prebuild master.

--- test/integration-napi-test.js
+++ test/integration-napi-test.js
@@ -18,32 +18,6 @@ test('can prebuild a gyp napi module for node', function (t) {
     t.end()
   })
 })
-/*
-test('can prebuild a gyp napi module for electron', function (t) {
-  rm.sync(path.join(cwd, 'prebuilds'))
-  var file = 'native-v1.0.0-electron-v50-' + process.platform + '-' + process.arch + '.tar.gz'
-  var prebuild = path.join(cwd, 'prebuilds', file)
-  // A quick, temporary fix for a node.js bug (https://github.com/prebuild/prebuild/pull/208#issuecomment-361108755)
-  console.log()
-  exec('npm run prebuild-electron', { cwd: cwd }, function (error, stdout, stderr) {
-    t.equal(error, null)
-    t.equal(fs.existsSync(prebuild), true)
-    t.end()
-  })
-})
-
-test('can prebuild a gyp napi module for node-webkit', function (t) {
-  rm.sync(path.join(cwd, 'prebuilds'))
-  var file = 'native-v1.0.0-node-webkit-v59-' + process.platform + '-' + process.arch + '.tar.gz'
-  var prebuild = path.join(cwd, 'prebuilds', file)
-  // A quick, temporary fix for a node.js bug (https://github.com/prebuild/prebuild/pull/208#issuecomment-361108755)
-  console.log()
-  exec('npm run prebuild-node-webkit', { cwd: cwd }, function (error, stdout, stderr) {
-    t.equal(error, null)
-    t.equal(fs.existsSync(prebuild), true)
-    t.end()
-  })
-}) */
 
 test('can prebuild a gyp napi module for node with prepack script', function (t) {
   rm.sync(path.join(cwd, 'prebuilds'))
