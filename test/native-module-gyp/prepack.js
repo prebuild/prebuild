6e30587701762864ce80f187c855721b5f663813
Vincent Weevers <mail@vincentweevers.nl>
2019-07-12 11:07:25 +0200

    Replace deprecated assert.equal with .strictEqual

--- test/native-module-gyp/prepack.js
+++ test/native-module-gyp/prepack.js
@@ -1,4 +1,4 @@
 var fs = require('fs')
 var assert = require('assert')
 
-assert.equal(fs.existsSync('./build/Release/native.node'), true)
+assert.strictEqual(fs.existsSync('./build/Release/native.node'), true)
