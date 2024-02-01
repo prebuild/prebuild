571867cc48330227c085427dd85013d1b2af5d5c
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2024-02-01 15:59:15 +0200

    Replace rimraf dev dependency with fs.rmSync (#319)

--- test/integration-cmake-test.js
+++ test/integration-cmake-test.js
@@ -2,12 +2,11 @@ var test = require('tape')
 var exec = require('child_process').exec
 var path = require('path')
 var fs = require('fs')
-var rm = require('rimraf')
 
 var cwd = path.join(__dirname, 'native-module-cmake')
 
 test('can prebuild a cmake-js native module for node', function (t) {
-  rm.sync(path.join(cwd, 'prebuilds'))
+  fs.rmSync(path.join(cwd, 'prebuilds'), { recursive: true, force: true })
   var file = 'native-v1.0.0-node-v57-' + process.platform + '-' + process.arch + '.tar.gz'
   var prebuild = path.join(cwd, 'prebuilds', file)
   exec('npm run prebuild', { cwd: cwd }, function (error, stdout, stderr) {
@@ -18,7 +17,7 @@ test('can prebuild a cmake-js native module for node', function (t) {
 })
 
 test('can prebuild a cmake-js native module for electron', function (t) {
-  rm.sync(path.join(cwd, 'prebuilds'))
+  fs.rmSync(path.join(cwd, 'prebuilds'), { recursive: true, force: true })
   var file = 'native-v1.0.0-electron-v50-' + process.platform + '-' + process.arch + '.tar.gz'
   var prebuild = path.join(cwd, 'prebuilds', file)
   exec('npm run prebuild-electron', { cwd: cwd }, function (error, stdout, stderr) {
@@ -29,7 +28,7 @@ test('can prebuild a cmake-js native module for electron', function (t) {
 })
 
 test('can prebuild a cmake-js native module for node with silent argument', function (t) {
-  rm.sync(path.join(cwd, 'prebuilds'))
+  fs.rmSync(path.join(cwd, 'prebuilds'), { recursive: true, force: true })
   var file = 'native-v1.0.0-node-v57-' + process.platform + '-' + process.arch + '.tar.gz'
   var prebuild = path.join(cwd, 'prebuilds', file)
   exec('npm run prebuild-silent', { cwd: cwd }, function (error, stdout, stderr) {
--- test/integration-gyp-test.js
+++ test/integration-gyp-test.js
@@ -2,12 +2,11 @@ var test = require('tape')
 var exec = require('child_process').exec
 var path = require('path')
 var fs = require('fs')
-var rm = require('rimraf')
 
 var cwd = path.join(__dirname, 'native-module-gyp')
 
 test('can prebuild a gyp native module for node', function (t) {
-  rm.sync(path.join(cwd, 'prebuilds'))
+  fs.rmSync(path.join(cwd, 'prebuilds'), { recursive: true, force: true })
   var file = 'native-v1.0.0-node-v57-' + process.platform + '-' + process.arch + '.tar.gz'
   var prebuild = path.join(cwd, 'prebuilds', file)
   exec('npm run prebuild', { cwd: cwd }, function (error, stdout, stderr) {
@@ -18,7 +17,7 @@ test('can prebuild a gyp native module for node', function (t) {
 })
 
 test('can prebuild a gyp native module for electron', function (t) {
-  rm.sync(path.join(cwd, 'prebuilds'))
+  fs.rmSync(path.join(cwd, 'prebuilds'), { recursive: true, force: true })
   var file = 'native-v1.0.0-electron-v50-' + process.platform + '-' + process.arch + '.tar.gz'
   var prebuild = path.join(cwd, 'prebuilds', file)
   exec('npm run prebuild-electron', { cwd: cwd }, function (error, stdout, stderr) {
@@ -29,7 +28,7 @@ test('can prebuild a gyp native module for electron', function (t) {
 })
 
 test.skip('can prebuild a gyp native module for node-webkit', function (t) {
-  rm.sync(path.join(cwd, 'prebuilds'))
+  fs.rmSync(path.join(cwd, 'prebuilds'), { recursive: true, force: true })
   var file = 'native-v1.0.0-node-webkit-v59-' + process.platform + '-' + process.arch + '.tar.gz'
   var prebuild = path.join(cwd, 'prebuilds', file)
   exec('npm run prebuild-node-webkit', { cwd: cwd }, function (error, stdout, stderr) {
@@ -40,7 +39,7 @@ test.skip('can prebuild a gyp native module for node-webkit', function (t) {
 })
 
 test('can prebuild a gyp native module for node with prepack script', function (t) {
-  rm.sync(path.join(cwd, 'prebuilds'))
+  fs.rmSync(path.join(cwd, 'prebuilds'), { recursive: true, force: true })
   var file = 'native-v1.0.0-node-v57-' + process.platform + '-' + process.arch + '.tar.gz'
   var prebuild = path.join(cwd, 'prebuilds', file)
   exec('npm run prebuild-prepack', { cwd: cwd }, function (error, stdout, stderr) {
